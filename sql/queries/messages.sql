5cc63b8da2b3cfa64fe9b10130dcae3dde48d348
Christian Toivola <c.toivola@gmail.com>
2026-02-22 14:19:46 -0500

    Adds missing DeleteMessage query

--- sql/queries/messages.sql
+++ sql/queries/messages.sql
@@ -9,3 +9,6 @@ FROM messages ORDER BY created_at ASC LIMIT ?;
 -- name: ListRecentMessages :many
 SELECT id, source, text, work_id, task_id, bead_id, event_type, created_at
 FROM messages ORDER BY created_at DESC LIMIT ?;
+
+-- name: DeleteMessage :execresult
+DELETE FROM messages WHERE id = ?;
